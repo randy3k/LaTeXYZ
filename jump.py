7ff80f26ee23764c54296f945ea7ba0ca8a3396e
Randy Lai <shingx1@gmail.com>
2013-10-09 00:57:30 -0700

    linux support

--- jump.py
+++ jump.py
@@ -11,8 +11,6 @@ class JumpToPdfCommand(sublime_plugin.TextCommand):
             return
 
         s = sublime.load_settings("Rubber.sublime-settings")
-        osx_settings = s.get("osx")
-        linux_settings = s.get("linux")
 
         keep_focus = args["keep_focus"] if "keep_focus" in args else True
         forward_sync = args["forward_sync"] if "forward_sync" in args else False
@@ -30,6 +28,8 @@ class JumpToPdfCommand(sublime_plugin.TextCommand):
 
         plat = sublime.platform()
         if plat == 'osx':
+            osx_settings = s.get("osx")
+
             args = ['osascript']
             apple_script = ('tell application "Skim"\n'
                                 'if '+ str(not keep_focus)+' then activate\n'
@@ -68,36 +68,28 @@ class JumpToPdfCommand(sublime_plugin.TextCommand):
 
         elif plat == 'linux':
 
+            linux_settings = s.get("linux")
             # the required scripts are in the 'evince' subdir
             ev_path = os.path.join(sublime.packages_path(), 'Rubber', 'evince')
             ev_fwd_exec = os.path.join(ev_path, 'evince_forward_search')
             ev_sync_exec = os.path.join(ev_path, 'evince_sync') # for inverse search!
-            #print ev_fwd_exec, ev_sync_exec
 
-            # Run evince if either it's not running, or if focus PDF was toggled
-            # Sadly ST2 has Python <2.7, so no check_output:
-            running_apps = subprocess.Popen(['ps', 'xw'], stdout=subprocess.PIPE).communicate()[0]
-            # If there are non-ascii chars in the output just captured, we will fail.
-            # Thus, decode using the 'ignore' option to simply drop them---we don't need them
-            running_apps = running_apps.decode(sublime_plugin.sys.getdefaultencoding(), 'ignore')
-
-            # Run scripts through sh because the script files will lose their exec bit on github
+            running_apps = subprocess.check_output(['ps', 'xw']).decode(sublime_plugin.sys.getdefaultencoding(), 'ignore')
 
             # Get python binary if set:
-            py_binary = linux_settings["python2"] or 'python'
-            sb_binary = linux_settings["sublime"] or 'sublime-text'
-            # How long we should wait after launching sh before syncing
-            sync_wait = linux_settings["sync_wait"] or 1.0
+            py_binary = linux_settings["python"] or 'python'
+            sb_binary = linux_settings["sublime"] or 'subl'
 
             evince_running = ("evince " + pdffile in running_apps)
             if (not keep_focus) or (not evince_running):
                 print("(Re)launching evince")
                 subprocess.Popen(['sh', ev_sync_exec, py_binary, sb_binary, pdffile], cwd=ev_path)
                 print("launched evince_sync")
-                if not evince_running: # Don't wait if we have already shown the PDF
-                    time.sleep(sync_wait)
+                if not evince_running:
+                    time.sleep(0.5)
             if forward_sync:
                 subprocess.Popen([py_binary, ev_fwd_exec, pdffile, str(line), srcfile])
+
         else:
             sublime.error_message("Platform not supported!")
 
