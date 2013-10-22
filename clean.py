6324125aa363c7e21d86db94efa251f84b92c583
Randy Lai <randy.cs.lai@gmail.com>
2013-10-21 23:50:07 -0700

    force clean build

--- clean.py
+++ clean.py
@@ -3,19 +3,23 @@ import os
 import re
 from . misc import *
 
-def cleantex(texfile):
-    prefix = os.path.splitext(texfile)[0]
-    ext = ["aux", "dvi", "lis", "log", "blg", "bbl", "toc", "idx", "ind",
-            "ilg", "thm", "out", "fdb_latexmk", "fls", "synctex.gz", "nav", "snm"]
-    # ls = os.listdir(dir)
-    # rexp = '|'.join(['\\.'+e for e in ext])
-    # fnames = [os.path.join(dir, f) for f in ls if re.search(rexp, f)]
-    for e in ext:
-        if os.path.isfile(prefix+'.'+e):
-            os.remove(prefix+'.'+e)
+def cleantex(texfile, force=False):
+    ext = [".aux", ".dvi", ".lis", ".log", ".blg", ".bbl", ".toc", ".idx", ".ind",
+            ".ilg", ".thm", ".out", ".fdb_latexmk", ".fls", ".synctex.gz", ".nav", ".snm"]
+    if force:
+        dir = os.path.dirname(texfile)
+        ls = os.listdir(dir)
+        rexp = "(" + '|'.join(['\\'+e for e in ext]) + ")$"
+        fnames = [os.path.join(dir, f) for f in ls if re.search(rexp, f)]
+        for f in fnames: os.remove(f)
+    else:
+        prefix = os.path.splitext(texfile)[0]
+        for e in ext:
+            if os.path.isfile(prefix+e):
+                os.remove(prefix+e)
 
 class LatexsqCleanCommand(sublime_plugin.TextCommand):
-    def run(self, edit):
+    def run(self, edit, force=False):
         view = self.view
         tex_root = get_tex_root(view)
         tex_dir = os.path.dirname(tex_root)
@@ -26,8 +30,12 @@ class LatexsqCleanCommand(sublime_plugin.TextCommand):
         texfiles = [f+".tex" if f[-4:].lower() != ".tex" else f for f in texfiles]
         texfiles = [tex_root] + [os.path.join(tex_dir,f) for f in texfiles]
         for f in texfiles:
-            if os.path.isfile(f): cleantex(f)
+            if os.path.isfile(f): cleantex(f, force)
 
-        print("Clean Build!")
-        sublime.status_message("Clean Build!")
+        if force:
+            print("Force Clean Build!")
+            sublime.status_message("Force Clean Build!")
+        else:
+            print("Clean Build!")
+            sublime.status_message("Clean Build!")
 
