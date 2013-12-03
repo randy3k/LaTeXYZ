c4598b9af50c9a04c010d9f73c024b461b72f86e
Randy Lai <randy.cs.lai@gmail.com>
2013-12-03 14:58:39 -0800

    better left right pair for \\{

--- autolrpair.py
+++ autolrpair.py
@@ -1,4 +1,5 @@
 import sublime, sublime_plugin
+import re
 
 # for auto pair \left \right pairs
 
@@ -12,7 +13,7 @@ class AutoLeftRightPairListener(sublime_plugin.EventListener):
             out = all([view.substr(sel.begin()-1)=='[' and view.substr(sel.end())==']' for sel in view.sel()])
             return (out == operand) if operator==0 else not (out == operand)
         elif key == 'in_curly_brackets':
-            out = all([view.substr(sel.begin()-1)=='{' and view.substr(sel.end())=='}' for sel in view.sel()])
+            out = all([view.substr(sublime.Region(sel.begin()-2,sel.begin()))=='\\{' and view.substr(sublime.Region(sel.begin(),sel.begin()+2))=='\\}' for sel in view.sel()])
             return (out == operand) if operator==0 else not (out == operand)
 
 class AutoLeftRightPairCommand(sublime_plugin.TextCommand):
@@ -21,11 +22,12 @@ class AutoLeftRightPairCommand(sublime_plugin.TextCommand):
         sel = [(s.begin(),s.end()) for s in view.sel()]
         view.sel().clear()
         for a,b in reversed(sel):
-            view.replace(edit, sublime.Region(a-1,b+1), view.substr(sublime.Region(a,b)))
-            view.sel().add(sublime.Region(a-1,b-1))
+            print(len(arg[0]))
+            view.replace(edit, sublime.Region(a-len(arg[0]),b+len(arg[1])), view.substr(sublime.Region(a,b)))
+            view.sel().add(sublime.Region(a-len(arg[0]),b-len(arg[0])))
 
-        left = "\\left"+arg[0]
-        right = "\\right"+arg[1]
+        left = "\\left"+ re.escape(arg[0])
+        right = "\\right"+ re.escape(arg[1])
 
         view.run_command("insert_snippet", {"contents": left+"${1:$SELECTION}"+right})
 
