49bbb81487a9de1f114f925da5029f3c0677a77e
Randy Lai <randy.cs.lai@gmail.com>
2016-03-21 10:41:52 -0400

    update linter settings only when not set

--- linter.py
+++ linter.py
@@ -5,6 +5,9 @@ def plugin_loaded():
     s = sublime.load_settings("SublimeLinter.sublime-settings")
     if s.has("user"):
         user = s.get("user")
-        user.get("syntax_map", {}).update({"latex+": "latex"})
-        s.set("user", user)
-        sublime.save_settings("SublimeLinter.sublime-settings")
+        syntax_map = user.get("syntax_map", {})
+        if "latex+" not in syntax_map:
+            syntax_map.update({"latex+": "latex"})
+            user["syntax_map"] = syntax_map
+            s.set("user", user)
+            sublime.save_settings("SublimeLinter.sublime-settings")
