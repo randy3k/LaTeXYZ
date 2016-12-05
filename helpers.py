6d7128d83f9fc321b244f8769a7a46478770d9d9
Randy Lai <randy.cs.lai@gmail.com>
2016-12-05 02:20:36 -0500

    improve click behaviour

--- helpers.py
+++ helpers.py
@@ -13,6 +13,10 @@ class LatexZetaInsertCommand(sublime_plugin.TextCommand):
 
 class LatexZetaJumpToPdfCommand(sublime_plugin.TextCommand):
     def run(self, edit):
+        pt = self.view.sel()[0].end() if len(self.view.sel()) > 0 else 0
+        if not self.view.score_selector(pt, "text.tex.latex"):
+            print("LaTeXZeta: current file is not a latex file.")
+            return
         self.view.window().run_command("view_pdf")
         # add a bit delay so that "view_pdf" is executed first
         sublime.set_timeout(
