63aa30f446124795ba62d1438fe55ef8acea51f6
Randy Lai <randy.cs.lai@gmail.com>
2017-08-24 01:25:38 -0400

    add settings inside class

--- query_context.py
+++ query_context.py
@@ -4,8 +4,6 @@ import sublime_plugin
 
 lz_settings_file = "LaTeXYZ.sublime-settings"
 
-lz_settings = sublime.load_settings(lz_settings_file)
-
 
 class LatexyzQuotes(sublime_plugin.EventListener):
 
@@ -15,5 +13,8 @@ class LatexyzQuotes(sublime_plugin.EventListener):
         if not view.match_selector(view.sel()[0].end() if len(view.sel()) > 0 else 0,
                                    "text.tex.latex"):
             return
+
+        lz_settings = sublime.load_settings(lz_settings_file)
+
         if key == 'latexyz_quotes':
             return lz_settings.get("use_latex_quotes", True)
