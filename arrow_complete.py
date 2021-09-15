abac83b44934db8f7774a669ebe34ce99f00e73b
Randy Lai <randy.cs.lai@gmail.com>
2021-09-15 10:56:33 -0700

    use our own command to complete arrows
    close #26

--- arrow_complete.py
+++ arrow_complete.py
@@ -21,23 +21,16 @@ arrow_map = {
 }
 
 
-class LatexyzArrowCompletions(sublime_plugin.EventListener):
-
-    general_commands = None
-    math_commands = None
-
-    def on_query_completions(self, view, prefix, locations):
-        if view.settings().get('is_widget'):
-            return
-
-        if not view.match_selector(locations[0], "text.tex.latex"):
-            return None
-
-        if not prefix:
-            # arrow completion
-            pt = locations[0]
+class LatexyzArrowCompleteCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        view = self.view
+        sel = view.sel()
+        for s in reversed(sel):
+            if not s.empty():
+                return
+            pt = s.end()
             arrow = view.substr(sublime.Region(pt - 4, pt))
             m = ARROW.search(arrow)
             if m and (m.group(1) or m.group(3)) and m.group(0) in arrow_map:
                 arr = arrow_map[m.group(0)]
-                return [(m.group(0), arr)]
+                view.replace(edit, sublime.Region(pt - len(m.group(0)), pt), arr)
